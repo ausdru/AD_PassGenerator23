5fea1a3a344cd136e394bd36c5635ada15a975a6
Austin <austindrury@live.com>
2023-09-10 15:04:52 -0600

    Finalized project for deployment.

---
+++ README.md
@@ -0,0 +1,37 @@
+## Password Generator 2023
+
+## Link
+
+
+## Description
+The motivation behind this project was to modify starter code to create an application that enables employees to generate random passwords based on criteria that they’ve selected. The app runs in the browser and features dynamically updated HTML and CSS powered by JavaScript code that I've written. It has a clean, polished, and responsive user interface that adapts to multiple screen sizes.
+
+## Usage
+![Screenshot1](ss1.png)
+![Screenshot2](ss2.png)
+
+## License
+
+MIT License
+
+Copyright (c) 2023 Austin Drury
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+-----------------------------------------------------------------
---
+++ script.js
@@ -0,0 +1,113 @@
+// Assigned code.
+Btn = document.querySelector("#generateBtn");
+var input;
+var includeNumbers = document.querySelector("#includeNumbers");
+var includeSymbols = document.querySelector("#includeSymbols");
+var includeUppercase = document.querySelector("#includeUppercase");
+var includeLowercase = document.querySelector("#includeLowercase");
+
+// Write password to password output.
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+}
+
+// Event listener.
+generateBtn.addEventListener('click', writePassword);
+
+// Prompt function for password criteria.
+function getPasswordCriteria(){
+
+// Prompt for password's length.
+do {
+  input = prompt('Please enter your preferred password length (8 - 128 Characters):');
+  lengthInput = parseInt(input);
+    if (isNaN(lengthInput)) {
+    alert('Invalid input! Please enter a valid number.'); }
+     else if (lengthInput < 8 || lengthInput > 128) {
+       alert('Invalid password length! Please enter a number between 8 and 128.');
+      }
+} while (isNaN(lengthInput) || lengthInput < 8 || lengthInput > 128);
+
+// Prompt for uppercase letters.
+do {
+  input = prompt('Would you like to include uppercase letters? (Enter Y for "Yes", or N for "No")').toUpperCase();
+  includeUppercase = input === 'Y';
+    if (!includeUppercase && input !== 'N') {
+    alert('Invalid input. Please enter Y for "Yes" or N for "No".'); 
+    }
+} while (input !== 'Y' && input !== 'N');
+
+// Prompt for lowercase letters.
+do {
+  input = prompt('Would you like to include lowercase letters? (Enter Y for "Yes", or N for "No")').toLowerCase();
+  includeLowercase = input === 'y';
+    if (!includeLowercase && input !== 'n') {
+    alert('Invalid input. Please enter Y for "Yes", or N for "No".');
+    }
+} while (input !== 'y' && input !== 'n');
+
+// Prompt for including numbers.
+do {
+  input = prompt('Would you like to include numbers? (Enter Y for "Yes", or N for "No")').toLowerCase();
+  includeNumbers = input === 'y';
+    if (!includeNumbers && input !== 'n') {
+    alert('Invalid input. Please enter Y for "Yes", or N for "No".');
+    }
+} while (input !== 'y' && input !== 'n');
+
+// Prompt for including symbols.
+do {
+  input = prompt('Would you like to include symbols? (Enter Y for "Yes", or N for "No")').toLowerCase();
+  includeSymbols = input === 'y';
+    if (!includeSymbols && input !== 'n') {
+    alert('Invalid input. Please enter Y for "Yes", or N for "No".');
+    }
+ } while (input !== 'y' && input !== 'n');
+
+  var password = generatePassword(lengthInput, includeUppercase, includeLowercase, includeNumbers, includeSymbols);
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+   
+// Function to generate password based on the criteria listed.
+function generatePassword(length, includeUppercase, includeLowercase, includeNumbers, includeSymbols) {
+
+// Defining character sets for each criteria type.
+  const uppercaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  const lowercaseChars = 'abcdefghijklmnopqrstuvwxyz';
+  const numberChars = '0123456789';
+  const symbolChars = '!@#$%^&*()_-+=<>?';
+
+// An empty string used to store characters used for password.
+   let validChars = '';
+   let password = '';
+
+// Adding charsets to the validChars string, based on the selected password criteria.
+   if (includeUppercase) {
+     validChars += uppercaseChars;
+    }
+   if (includeLowercase) {
+     validChars += lowercaseChars;
+    }
+   if (includeNumbers) {
+     validChars += numberChars;
+    }
+   if (includeSymbols) {
+     validChars += symbolChars;
+    }
+
+// Ensuring at least one character type is selected.
+   if (validChars === '') {
+     alert('Please choose at least one character type.');
+     return '';
+    }
+
+// Password generates via selecting random characters from validChars.
+   for (let i = 0; i < length; i++) {
+     const randomIndex = Math.floor(Math.random() * validChars.length);
+     password += validChars.charAt(randomIndex);
+    }
+  return password;
+  }
+}
